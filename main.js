b10453340a97ca941ddd875acf8873fc3fbb33a9
GoitseoneKau <66080430+GoitseoneKau@users.noreply.github.com>
2024-04-08 12:12:08 +0200

    first portfolio

---
+++ main.js
@@ -0,0 +1,20 @@
+let nav = document.getElementById("navList");
+let btnNav = document.getElementById("menuBurger");
+
+btnNav.onclick =function(){
+    nav.classList.toggle("show");
+    btnNav.classList.toggle("btnActive");
+};
+
+var map = new ol.Map({
+layers: [
+  new ol.layer.Tile({
+    source: new ol.source.OSM()
+  })
+],
+target: 'map',
+view: new ol.View({
+  center: [13,4737, -4703],
+  zoom: 5
+})
+});
