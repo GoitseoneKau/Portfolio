cbf829bbe913326dbc383784991495d489fa2067
GoitseoneKau <66080430+GoitseoneKau@users.noreply.github.com>
2024-04-22 08:42:17 +0200

    unfinshed validation

---
+++ about.js
@@ -0,0 +1,27 @@
+window.onload=()=>{
+    //For About Page
+//about-certificates js for Qualifications
+let certificateLinks = {
+    "Beginner in Web Development &nbsp &nbsp &nbsp Module 1(<b>HTML,CSS,Design</b>)":
+    "https://www.upskillist.com/certificate/en/XkZcVyzh/unh0m8xa56lrab1awypunh0z40cp9uww",
+  
+    "Intermediate in Web Development  Module 2(<b>Javascript,UI/UX</b>)":
+    "https://www.upskillist.com/certificate/en/nJlA494U/93f0qy0bbu8l15f9n793f0qy0krj5y6i",
+  
+    "Advanced in Web Development &nbsp &nbsp &nbspModule 3(<b>PHP & MySQL</b>)":
+    "https://www.upskillist.com/certificate/en/tT7CRe7l/el10g9oga5r091xokel10g9sglqod9tz",
+  
+    "Proficient in Web Development &nbsp&nbsp&nbsp&nbsp Module 4(<b>CSS,HTML,PHP & MySQL,JS/PHP Frameworks</b>)":
+    "https://www.upskillist.com/certificate/en/dEnQaPgH/yy9v5ye2hhpsykjay0wmzyy9v58m0fb3"
+  };
+  
+  
+  let certificatesParent = document.getElementsByClassName("certificates");
+  
+  Object.keys(certificateLinks).forEach(key=>{
+    let certificate = `<a href="${certificateLinks[key]}">${key}</a>`;
+    let certList = document.createElement("li");
+    certList.innerHTML = certificate;
+    certificatesParent.item(0).appendChild(certList);
+  })
+}
--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-//nav js
+
+
+window.onload=()=>{
+  //nav js
 let nav = document.getElementById("navList");
 let btnNav = document.getElementById("menuBurger");
 
@@ -6,7 +9,7 @@ btnNav.onclick =function(){
     nav.classList.toggle("show");
     btnNav.classList.toggle("btnActive");
 };
-
+/*
 //map js
 var map = new ol.Map({
 layers: [
@@ -19,29 +22,131 @@ view: new ol.View({
   center: [13,4737, -4703],
   zoom: 5
 })
-});
-
-//about-certificates js
-let certificateLinks = {
-  "Beginner in Web Development &nbsp &nbsp &nbsp Module 1(<b>HTML,CSS,Design</b>)":
-  "https://www.upskillist.com/certificate/en/XkZcVyzh/unh0m8xa56lrab1awypunh0z40cp9uww",
-
-  "Intermediate in Web Development  Module 2(<b>Javascript,UI/UX</b>)":
-  "https://www.upskillist.com/certificate/en/nJlA494U/93f0qy0bbu8l15f9n793f0qy0krj5y6i",
-
-  "Advanced in Web Development &nbsp &nbsp &nbspModule 3(<b>PHP & MySQL</b>)":
-  "https://www.upskillist.com/certificate/en/tT7CRe7l/el10g9oga5r091xokel10g9sglqod9tz",
-
-  "Proficient in Web Development &nbsp&nbsp&nbsp&nbsp Module 4(<b>CSS,HTML,PHP & MySQL,JS/PHP Frameworks</b>)":
-  "https://www.upskillist.com/certificate/en/dEnQaPgH/yy9v5ye2hhpsykjay0wmzyy9v58m0fb3"
-};
+});*/
 
 
-let certificatesParent = document.getElementsByClassName("certificates");
 
-Object.keys(certificateLinks).forEach(key=>{
-  let cert = `<a href="${certificateLinks[key]}">${key}</a>`;
-  let certli = document.createElement("li");
-  certli.innerHTML = cert;
-  certificatesParent.item(0).appendChild(certli);
-})
+//For Contact Me Page
+//Form
+let form = document.getElementById("contactForm");
+let lastName =  document.getElementById("lname");
+let firstName = document.getElementById("fname");
+  let email = document.getElementById("email");
+  let message = document.getElementById("message");
+  let btnSubmit = document.getElementById("submit");
+
+  //disable submit button
+  btnSubmit.disabled = true;
+  btnSubmit.style.opacity = 0.6;
+
+//validity styles
+  let validGreen = " 0 0 15px green";
+  let invalidRed = " 0 0 15px red";
+
+//email
+email.onblur = () => {
+  let pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  let isvalidEMail = email.value.trim() !==""  && pattern.test(email.value);
+  let alert = document.createElement("p");
+  alert.id="alert";
+  
+ 
+  if (isvalidEMail) {
+    email.style.boxShadow = validGreen;
+    if(email.parentElement.lastChild.nodeValue=="not valid"){email.parentElement.lastChild.remove();}
+  
+  } else {
+    email.style.boxShadow = invalidRed;
+    alert.innerText ="not valid";
+    email.parentElement.appendChild(alert);
+  }
+}
+
+//message
+message.onblur = () => {
+  let isvalidMessage = message.value.trim() !=="";
+  if (isvalidMessage) {
+    message.style.boxShadow = validGreen;
+  } else {
+    message.style.boxShadow = invalidRed;
+  }
+}
+
+//first name
+firstName.onblur = () => {
+  let isvalidFname = firstName.value.trim() !=="";
+  if (isvalidFname) {
+    firstName.style.boxShadow = validGreen;
+  } else {
+    firstName.style.boxShadow = invalidRed;
+  }
+}
+//last name
+lastName.onblur = () => {
+  let isvalidLname = lastName.value !=="";
+  if (isvalidLname==true) {
+    lastName.style.boxShadow = validGreen;
+  
+  } 
+  if(isvalidLname==false) {
+    lastName.style.boxShadow = invalidRed;
+
+  }
+}
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  
+  //validity 
+  let pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  let isvalidEMail = email.value.trim() !==""  && pattern.test(email.value);
+  let isvalidMessage = message.value.trim() !=="";
+  let isvalidFname = firstName.value.trim() !=="";
+  let isvalidLname = lastName.value !=="";
+  let isvalidForm;
+
+  let alert = document.createElement("span");
+    alert.innerText ="not valid";
+  if (isvalidEMail) {
+    alert.innerText ="valid";
+    email.parentElement.appendChild(alert);
+    
+  } else {
+   
+    email.parentElement.appendChild(alert);
+  }
+
+
+  /*if (allvalid) {
+    
+  } else {
+    var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your submission!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+  }*/
+}
+//add function to event handler 
+form.addEventListener("submit", handleSubmit)
+
+
+}
