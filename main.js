9482c5055e2d8f6cce4f11c29ba1da21bb002b30
GoitseoneKau <66080430+GoitseoneKau@users.noreply.github.com>
2024-04-25 11:13:24 +0200

    style update

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ btnNav.onclick =function(){
     nav.classList.toggle("show");
     btnNav.classList.toggle("btnActive");
 };
-/*
+
 //map js
 var map = new ol.Map({
 layers: [
@@ -22,7 +22,35 @@ view: new ol.View({
   center: [13,4737, -4703],
   zoom: 5
 })
-});*/
+});
+
+
+let address = document.getElementById("address");
+let addressHTML =`<h4>Address :</h4>
+Mabopane<br>
+Pretoria, Gauteng, 0190 <br>
+South Africa`;
+address.innerHTML = addressHTML;
+//social media
+let awesomeIcons =["fa-brands fa-square-whatsapp fa-2xl","fa-brands fa-linkedin fa-2xl","fa-brands fa-square-github fa-2xl"];
+
+let social_links = {
+"https://wa.me/+27812702638":awesomeIcons[0],
+"https://www.linkedin.com/in/goitseone-kau-65752511a/":awesomeIcons[1],
+"https://github.com/GoitseoneKau":awesomeIcons[2]
+};
+
+
+let socialMedia = document.getElementById("social-media");
+
+Object.keys(social_links).forEach((links)=>{
+  let a = document.createElement("a");
+  let i = document.createElement("i");
+  i.classList += social_links[links];
+  a.href=links;
+  a.appendChild(i);
+  socialMedia.appendChild(a);
+});
 
 
 
